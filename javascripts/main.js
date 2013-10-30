2e0f85deeed438507e51d659847aec2697be4cf2
Gustavo Machado <machadogj@gmail.com>
2013-10-30 18:13:39 -0300

    moving events to a json file for easy editing, using handlebars for templating

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,24 @@
-//initialize localizr
-var locale = localizr({ default: 'en', onChanged: setSelected});
 
-function setSelected (lang) {
-	$(".language-selector > li").removeClass("selected");
-	$(".language-selector ." + lang).addClass("selected");
-}
 
-$(".language-selector > li").click(function () {
-	locale.setLocale($(this).html());
+//load the events from a json obj
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+$.ajax({url: 'javascripts/events.json'}).done(function (events) {
+	var $list = $("#events-list");
+	$list.html('');
+	$.each(events, function (i, e) {
+		$list.append(template(e));
+	});
+	//after the content was generated, localize it
+	//initialize localizr
+	var locale = localizr({ default: 'en', onChanged: setSelected});
+
+	function setSelected (lang) {
+		$(".language-selector > li").removeClass("selected");
+		$(".language-selector ." + lang).addClass("selected");
+	}
+
+	$(".language-selector > li").click(function () {
+		locale.setLocale($(this).html());
+	});
 });
