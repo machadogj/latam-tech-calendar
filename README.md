ce80bff84736886dc361c6273e96f9a552098811
Gustavo Machado <machadogj@gmail.com>
2013-10-30 13:41:20 -0300

    adding info to readme, and cleanup of localizr.js

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 #Latin America Tech Events Calendar
 
 The purpose of this repository is to organize a single page with the best Latin America Technology Events, and help spread the word.
+
+#Editing the Content
+
+The site uses a series of JSON objects to localize the site in different
+languages. In order to edit the content, please refer to the files in
+`javascripts/locale/[lang].json`.
+
+The JSON files uses `css selectors` in order to find the right place to
+insert the content.
--- javascripts/lib/localizr.js
+++ javascripts/lib/localizr.js
@@ -1,9 +1,28 @@
-function localizr (opts) {
+/**
+ * localizr - very simple script to localize a site in different languages.
+ * Author: Gustavo Machado
+ * Date: 10/29/2013
+ *
+ * Usage:
+ *      var l = localizr({ default: 'es', onChanged: setSelected});
+ *      l.setLocale('en'); //changes the language
+ * @param {object} opts
+ *      - {string} default language
+ *      - {object} dics of languages
+ *      - {fn}     onChanged callback triggered when laguange changed
+ *      - {string} path to the language dictionaries
+ * @return {object}
+ * @api public
+ */
+
+function localizr ( opts ) {
+
     // default options
-    opts = opts || {};
-    opts.default = opts.default || "en";
-    opts.dics = opts.dics || {};
-    opts.onChanged = opts.onChanged || function () {};
+    opts           = opts           || {};
+    opts.default   = opts.default   || "en";
+    opts.dics      = opts.dics      || {}; //used to cache the dics
+    opts.onChanged = opts.onChanged || function noop() {};
+    opts.path      = opts.path      || 'javascripts/locale/';
 
     // get the default language from local storage
     var locale = localStorage.getItem("locale") || opts.default;
@@ -11,24 +30,30 @@ function localizr (opts) {
     setLocale(locale);
 
     //function for setting the locale in the localStorage
+    /**
+     * setLocale - sets the language.
+     * @param {string} lang to be used
+     * @api private
+     */
     function setLocale (lang) {
         
+        //save the language in the localStorage
         localStorage.setItem('locale', lang);
 
-            // go through the dom searching for keys in the dictionary
+        // go through the dom searching for keys in the dictionary
         // and set the HTML content for it.
         function applyDictionary (dic) {
             for(var i in dic) {
                 $(i).html(dic[i]);
             }
         }
-        // either the load the dictionary from memory or ajax.
+        // either load the dictionary from memory or ajax.
         if (opts.dics[lang]) {
             applyDictionary(opts.dics[lang]);
             opts.onChanged(lang);
         }
         else {
-            $.ajax({url:'javascripts/locale/' + lang + '.json'})
+            $.ajax({url: opts.path + lang + '.json'})
                 .done(function (data) {
                     opts.dics[lang] = data;
                     applyDictionary(data);
@@ -41,7 +66,6 @@ function localizr (opts) {
         }
     }
 
-    // return an object with configuration.
     return {
         setLocale: setLocale
     };
